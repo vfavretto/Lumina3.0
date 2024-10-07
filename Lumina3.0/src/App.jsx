7dfca275006298338257b758d6ecdec242a70f15
vfavretto <vf.favretto@gmail.com>
2024-10-07 12:19:21 -0300

    Fix: Profile page

--- Lumina3.0/src/App.jsx
+++ Lumina3.0/src/App.jsx
@@ -1,12 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/common/header';
-import Home from './components/pages/home';
-import About from './components/pages/about';
-import Partners from './components/pages/partners';
-import Blog from './components/pages/blog';
-import Login from './components/pages/login.jsx';
-import Footer from './components/common/footer';
-import Profile from './components/pages/profile.jsx';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/common/header";
+import Home from "./components/pages/home";
+import About from "./components/pages/about";
+import Partners from "./components/pages/partners";
+import Blog from "./components/pages/blog";
+import Login from "./components/pages/login.jsx";
+import Footer from "./components/common/footer";
+import Profile from "./components/pages/profile.jsx";
 
 function App() {
   return (
@@ -21,9 +21,9 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
-   <Footer />
+      <Footer />
     </Router>
   );
-};
+}
 
 export default App;
