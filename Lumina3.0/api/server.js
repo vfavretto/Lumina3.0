ffca2e1a5df4e8b499cd49180e945f1aa9779eb7
Zan-Kir <murilo4santos@hotmail.com>
2024-10-15 19:30:43 -0300

    last test

---
+++ Lumina3.0/api/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import connectDB from './config/database.js';
+import authRoutes from './routes/userRoutes.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+app.use(cors());
+
+connectDB();
+
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ Lumina3.0/api/userRoutes.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import userController from '../controllers/userController.js';
+
+const router = express.Router();
+
+router.post('/register', userController.register);
+router.post('/login', userController.login);
+router.get('/check', userController.checkAuth);
+router.get('/user/:id', userController.getUser);
+router.put('/user/:id', userController.updateUser);
+router.delete('/user/:id', userController.deleteUser);
+
+export default router;
