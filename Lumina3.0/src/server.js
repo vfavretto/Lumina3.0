2fed83491ea82d63ca1b1e30e1a143a504543854
vfavretto <vf.favretto@gmail.com>
2024-10-15 18:49:06 -0300

    Fix: vercel code

--- Lumina3.0/src/server.js
+++ Lumina3.0/src/server.js
@@ -16,5 +16,6 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
