75524d25b13e6d4675746b5afe700432fb70d646
Zan-Kir <murilo4santos@hotmail.com>
2024-10-16 00:53:43 -0300

    config test

--- Lumina3.0/vite.config.js
+++ Lumina3.0/vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist',
+    emptyOutDir: true,
     rollupOptions: {
-      input: '/index.html'
+      input: 'index.html'
     }
   }
 })
