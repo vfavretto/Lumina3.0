0ef13fa5301248f5f3404ea331fb29ec16fd829c
Júlia Soares <juliasoares.contato@gmail.com>
2024-10-18 17:38:19 -0300

    fix #20

--- Lumina3.0/src/views/pages/register.jsx
+++ Lumina3.0/src/views/pages/register.jsx
@@ -6,45 +6,44 @@ const Register = () => {
   return (
 <main className="profile-page">
 <div className="container">
-<h1>Complete Seu Perfil</h1>
-<form>
+<h1 className="titleProfile">Complete Seu Perfil</h1>
+<form className='formRegister'>
 <section className="representative-info">
 
-<h2>Cadastro Representante</h2>
-<div class="container text-center">
-  <div class="row align-items-start">
-    <div class="col">
+<h2 className='titleProfileInner'>Cadastro Representante</h2>
+<div className="container text-center">
+  <div className="row align-items-start">
+    <div className="col">
     <input type="text" placeholder="Nome" />
     </div>
     </div>
   </div>
 
-  <div class="container text-center">
-  <div class="row align-items-start">
-    <div class="col">
+  <div className="container text-center">
+  <div className="row align-items-start">
+    <div className="col">
     <input type="email" placeholder="E-mail" />
     </div>
     </div>
   </div>
 
-  <div class="container text-center">
-  <div class="row align-items-start">
-    <div class="col">
+  <div className="container text-center">
+  <div className="row align-items-start">
+    <div className="col">
     <input type="tel" placeholder="Telefone" />    </div>
-    <div class="col">
+    <div className="col">
     <input type="text" placeholder="CPF" />    </div>
   </div>
   </div>
 
 </section>
- 
-          <section className="company-info">
-<h2>Cadastro Empresa</h2>
-<div class="container text-center">
-  <div class="row align-items-start">
-    <div class="col">
+<section className="company-info">
+<h2 className='titleProfileInner'>Cadastro Empresa</h2>
+<div className="container text-center">
+  <div className="row align-items-start">
+    <div className="col">
     <input type="text" placeholder="Nome" />    </div>
-    <div class="col-md-3">
+    <div className="col-md-3">
     <select>
 <option value="">Empresa</option>
 <option value="">Fornecedor</option>
@@ -55,70 +54,70 @@ const Register = () => {
   </div>
 
 
-  <div class="container text-center">
-  <div class="row align-items-start">
-    <div class="col">
+  <div className="container text-center">
+  <div className="row align-items-start">
+    <div className="col">
     <input type="email" placeholder="E-mail Empresa" />
     </div>
-    <div class="col">
+    <div className="col">
     <input type="url" placeholder="Site" />
     </div>
     </div>
   </div>
 
-  <div class="container text-center">
-  <div class="row align-items-start">
-    <div class="col">
+  <div className="container text-center">
+  <div className="row align-items-start">
+    <div className="col">
     <input type="tel" placeholder="Telefone" />
     </div>
-    <div class="col">
+    <div className="col">
     <input type="text" placeholder="CNPJ" />
     </div>
     </div>
   </div>
 
-  <div class="container text-center">
-  <div class="row align-items-start">
-    <div class="col">
+  <div className="container text-center">
+  <div className="row align-items-start">
+    <div className="col">
     <input type="text" placeholder="Endereço Comercial" />
     </div>
-    <div class="col-md-3">
+    <div className="col-md-3">
     <input type="text" placeholder="CEP" />
     </div>
     </div>
   </div>
 
-  <div class="container text-center">
-  <div class="row align-items-start">
-    <div class="col">
+  <div className="container text-center">
+  <div className="row align-items-start">
+    <div className="col">
     <input type="text" placeholder="Cidade" />
     </div>
-    <div class="col">
+    <div className="col">
     <input type="text" placeholder="UF" />
     </div>
-    <div class="col">
+    <div className="col">
     <input type="text" placeholder="País" />
     </div>
     </div>
   </div>
-  <div class="container text-center">
+  <div className="container text-center">
 <textarea placeholder="Descrição Empresa"></textarea>
 </div>
 
 </section>
  
           <section className="social-media">
-<h2>Redes Sociais</h2>
+<h2 className='titleProfileInner'>Redes Sociais</h2>
 
-<div class="container text-center" className="formRedes">
-  <div class="row align-items-start" >
-    <div class="col">
+<div className="container text-center" id="formRedes">
+  <div className="row align-items-start" >
+    <div className="col">
     <input type="url" placeholder="LinkedIn" />
     </div>
-    <div class="col">
+    <div className="col">
     <input type="url" placeholder="Instagram"  />
     </div>
-    <div class="col">
+    <div className="col">
     <input type="url" placeholder="Facebook" />
     </div>
   </div>
